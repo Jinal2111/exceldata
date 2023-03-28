c3b1b19c547e51654356278403d3d0257a812b6a
thanos <mr.jadhav721@gmail.com>
2023-03-28 14:29:54 +0530

    thanos commit

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-const express = require('express')
-
+const express = require('express');
 const app = express();
+require('./config/database');
 
-require('./connection/db')
+app.use('/api', require('./routes/index'));
 
-const path = require('path')
-
-require('dotenv').config()
-
-const routes = require('./routes/route')
-
-app.use('/',routes)
-const port = process.env.PORT || 3000
-
-
-// const name = xlsx.utils.sheet_to_json(data.Sheets[sheet_name], { header: ['Name',,'Marks']})
-
-
-app.listen(port, () => {
-    console.log(`shuru ho gaya ${port} pe`)
-})
+app.listen(process.env.PORT, () => console.log('listening at port'))  
