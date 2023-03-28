3daba6950f5aeb153416d5942a720a7d84306947
jinal <jinal2111@gmail.com>
2023-03-28 13:04:31 +0530

    jinal commit

--- controller/controller.js
+++ controller/controller.js
@@ -3,13 +3,14 @@ const studentmodel = require('../model/student');
 
 exports.student = async (req, res) => {
     try{
-        const excel = xlsx.readFile('./studentdata.xlsx');
+        const excel = xlsx.readFile('./studentdata.xlsx', { sheetRows : 4 });
         const data = xlsx.utils.sheet_to_json(excel.Sheets['Sheet1']);
+        console.log(data);
         data.forEach(async (i) => {
             const student = new studentmodel(i);
             await student.save();
         })
-        res.status(200).send("data successfully store in database");
+        res.status(200).send("data successfully added in database");
     }
     catch (err) {
         res.status(500).send("something went wrong..")
--- migrations/20230327084534-student.js
+++ migrations/20230327084534-student.js
@@ -11,7 +11,7 @@ module.exports = {
     // TODO write your migration here.
     // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+       await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
   },
 
   async down(db, client) {
